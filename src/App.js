613af053c3f5afdf4988d208592c44540bb81f59
LamLeTomat <61313056+LamLeTomat@users.noreply.github.com>
2021-04-20 15:25:37 +0700

    Todo app ReactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react';
+import TodoList from './components/TodoList';
+import Textfield from '@atlaskit/textfield';
+import Button from '@atlaskit/button';
+import {v4} from 'uuid';
+
 
 function App() {
+  //Nội dung người dùng nhập vào Input
+  const onTextInputChange = useCallback((e) => {
+  setTextInput(e.target.value);
+  }, []);
+
+  const onAddButtonClick = (e) => {
+    //Thêm text input vào danh sách todo list
+    //C1: Truyền trực tiếp vào setTodoList
+    // setTodoList([{name: 'Item 1'}]);
+  
+    setTodoList([
+      {id: v4(), name: textInput, isCompleted: false},
+      ...todoList, 
+      
+
+    ]);
+
+    setTextInput("");
+  };
+
+  const onCheckBtnClick = useCallback((id) => {
+    setTodoList(prevState => 
+      prevState.map(todo => 
+        todo.id ===id ? {...todo, isComplete: true} : todo))
+  },[]);
+  // state, props
+  // React Hook useState: Nhận vào 1 tham số làm giá trị khởi tạo
+  const[todoList, setTodoList] = useState([]); //useState trả về 1 array với 2 element; 1 biến để lưu trữ state, 1 method để update
+  //THêm 1 state để lưu trữ giá trị vừa nhập, giá trị mặc định là empty string
+  const[textInput, setTextInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Danh sách cần làm</h3>
+      <Textfield 
+      name="add-todo" 
+      aria-label="default text field" 
+      placeholder="Thêm việc làm" 
+      elemAfterInput={
+        <Button isDisabled={!textInput} appearance='primary' onClick={onAddButtonClick}>Thêm</Button>
+      }
+      css={{padding: '2px 4px 2px'}}
+      value={textInput}
+      onChange = {onTextInputChange}
+      />
+      <TodoList todoList={todoList} onCheckBtnClick={onAddButtonClick} />
     </div>
   );
 }
